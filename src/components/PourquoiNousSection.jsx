9609be14f2317771809a2468570d66a0d2032b8e
Emilien <emilien.marotta@gmail.com>
2025-04-18 22:32:36 +0200

    Optimisation accessibilité

--- src/components/PourquoiNousSection.jsx
+++ src/components/PourquoiNousSection.jsx
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 export default function PourquoiNousSection({
     img,
     title,
-    desc
+    desc,
+    alt
 }) {
     return (
         <div className="flex flex-1 flex-col items-center gap-4 max-w-[500px] rounded-lg bg-paletteColor3 p-4 border border-borderColor text-center">
-            <img className="size-10" src={img}/>
+            <img className="size-10" src={img} alt={alt}/>
             <h3>{title}</h3>
             <p className="small-text text-smTextWhtColor">{desc}</p>
         </div>
@@ -19,4 +20,5 @@ PourquoiNousSection.propTypes = {
     img: PropTypes.string.required,
     title: PropTypes.string.required,
     desc: PropTypes.string.required,
+    alt: PropTypes.string.required,
 }
